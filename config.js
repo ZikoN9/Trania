7440c76801c5e0f092ad233091b2be46c7ec440d
Freekill909 <109369361+Freekillbio@users.noreply.github.com>
2024-04-06 17:23:26 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,1 @@
-var wbhk = 'https://discord.com/api/webhooks/1226126479461711923/oyrugegQKBcQFI8HFajMZGKJ9EViCuOK42DsoCGcw3Jp-i2c_gHXKKf8KN_5SjFtMrTA'
-var regions = 'IP_DEFAULT_REGION'; // Dont Delete/Change Here !
+
