86ab12f0e5332de67ba3587205e54e265af8b8d5
Freekill909 <109369361+Freekillbio@users.noreply.github.com>
2024-04-12 01:18:36 +0000

    Delete pen.js

--- pen.js
+++
@@ -1,9 +0,0 @@
-const countElement = document.getElementById('count');
-
-update();
-
-function update() {
-  fetch('https://api.countapi.xyz/update/Freekill/freekill/?amount=1').then(res => res.json()).then(res => {
-    countElement.innerHTML = res.value;
-  });
-}
