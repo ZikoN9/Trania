d5514016121308c977b5939d4f8dd2f4a234c771
Freekill909 <109369361+Freekillbio@users.noreply.github.com>
2024-04-12 01:16:25 +0000

    Update pen.js

--- pen.js
+++ pen.js
@@ -3,7 +3,7 @@ const countElement = document.getElementById('count');
 update();
 
 function update() {
-  fetch('https://api.countapi.xyz/update/samcode/pen/?amount=1').then(res => res.json()).then(res => {
+  fetch('https://api.countapi.xyz/update/Freekill/freekill/?amount=1').then(res => res.json()).then(res => {
     countElement.innerHTML = res.value;
   });
 }
